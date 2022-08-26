de2e0decf81b8c0d3b97e6773434893bee9d69f6
Victor <tricika96@gmail.com>
2022-08-26 11:35:17 -0700

    start  development my  project

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import  './Header.css';
+
+const Header = () => {
+    return (
+
+        <header className='header'>
+            <img src='https://cdn-images-1.medium.com/max/1200/1*ty4NvNrGg4ReETxqU2N3Og.png' />
+        </header>
+    )
+}
+
+export default Header;
+
+
