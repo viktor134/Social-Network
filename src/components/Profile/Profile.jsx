495b489fd2dc8701f8e1cee15ff386b9bdec92eb
Victor <tricika96@gmail.com>
2022-09-25 11:30:53 -0700

    update ui

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,7 +6,7 @@ const Profile = () => {
     return (
     <div>
 
-        <img src ='https://thurrott.s3.amazonaws.com/wp-content/uploads/sites/2/2022/01/14172326/netflix.jpg' alt=''/>
+        <img src ='assets/profile/Raster.jpeg' alt=''/>
 
         <div>
             ava + description
